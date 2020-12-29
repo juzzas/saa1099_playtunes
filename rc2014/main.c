eef7b2ba3273afeed0d533e16508afcff29cb41b
Justin Skists <justin.skists@juzza.co.uk>
2020-12-29 15:33:56 +0000

    added RC2014 support

---
+++ rc2014/main.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <z80.h>
+
+#include "SAATunes.h"
+#include "score.h"
+
+static struct SAATunesContext L_context;
+
+int main(void)
+{
+    __asm__ ("di");
+
+    SAATunesInit(&L_context);
+
+    SAATunesStrum(&L_context);
+
+    SAATunesPlayScore(&L_context, score_auld_lang_syne);
+
+    while(1)
+    {
+        z80_delay_ms(1);
+        SAATunesTick(&L_context);
+    }
+
+
+    __asm__ ("infinite: jr infinite");
+    return 0;
+}
